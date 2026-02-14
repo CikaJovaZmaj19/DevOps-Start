bf7a88d519150a123a71c3e58c8bc9ab047a59d9
CikaJovaZmaj19 <markojova19@gmail.com>
2026-02-14 22:04:49 +0100

    Files and secrets fixed

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Zdravo! Python aplikacija radi unutar Docker-a! 🚀</h1>'
+    return '<h1>Hello! Python application works within Docker! 🚀</h1>'
 
 if __name__ == "__main__":
-    # Port mora biti 0.0.0.0 da bi bio dostupan van kontejnera
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
